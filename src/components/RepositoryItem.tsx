9acce67c40fce423c413effef5f3e4fd3b5f8071
Renan Martins <renan.silva@graebert.com>
2021-09-27 15:08:15 -0300

    Change project to use typescript

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,21 @@
+interface RepositoryItemProps {
+	repository: {
+		name: string;
+		description: string;
+		html_url: string;
+	}
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+	const repository = props.repository ?? {};
+	
+	return (
+		<li>
+			<strong>{repository.name}</strong>
+			<p>{repository.description}</p>
+			<a href={repository.html_url}>
+				Acessar repositório
+			</a>
+		</li>
+	)
+}
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,21 +3,20 @@ import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
+interface Repository {
+	name: string;
+	description: string;
+	html_url: string;
+}
+
 export function RepositoryList() {
-	const [repositories, setRepositories] = useState([]);
+	const [repositories, setRepositories] = useState<Repository[]>([]);
 	
 	useEffect(() => {
 		fetch('https://api.github.com/users/reenanms/repos')
 			.then(response => response.json())
 			.then(data => 
-				setRepositories(
-					data.map(d => ({
-							name: d.name,
-							description: d.description,
-							link: d.html_url
-						})
-					)
-				)
+				setRepositories(data)
 			);
 	}, [])
 
